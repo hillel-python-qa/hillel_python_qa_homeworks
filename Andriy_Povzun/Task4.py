09bcd0717839bb6bf6d64188d2c844fc1db94397
AndriyPovzun <povzuun99@gmail.com>
2022-08-24 15:11:34 +0300

    add_new_line

--- Andriy_Povzun/Task4.py
+++ Andriy_Povzun/Task4.py
@@ -4,3 +4,4 @@ common_room = ['Conor McGregor', 'Jose Aldo', 'Lionel Messi',
                'Bradly Cooper', ' Tom Hardy', 'Johny Depp']
 print(f'Vip guests: {vip_box}. \n'
       f'Guests in common room: {common_room}.')
+
