511c85433e347ffad2daccd9bb27bd0b11a841df
AndriyPovzun <povzuun99@gmail.com>
2022-08-26 18:06:58 +0300

    Finish_second_task

--- Andriy_Povzun/HW4_Andriy_Povzun/Second_Task.py
+++ Andriy_Povzun/HW4_Andriy_Povzun/Second_Task.py
@@ -3,10 +3,10 @@ number = 2
 first_way_multiply = 2
 first_way_multiply *= number
 
-second_way_multiply = number * 2
+second_way_multiply = number << 1
 
 first_way_devide = 2
 first_way_devide /= number
 
-second_way_devide = number / 2
+second_way_devide = number >> 1
 
