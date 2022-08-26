14777120b22f051dca4102bda10013abcefb671b
AndriyPovzun <povzuun99@gmail.com>
2022-08-26 18:12:31 +0300

    Done_HW

--- Andriy_Povzun/HW4_Andriy_Povzun/Third_Task.py
+++ Andriy_Povzun/HW4_Andriy_Povzun/Third_Task.py
@@ -2,10 +2,10 @@ friends = ["John", "Marta", "James"]
 enemies = ["John", "Johnatan", "Artur"]
 
 for friend in friends:
-    if friend == 'James':
+    if friend in enemies:
+        print(f"We with {friend} are not the friends anymore")
+    elif friend == "James":
         continue
-    elif friend in enemies:
-        print(f'We with {friend} are not the friends anymore')
     else:
-        print(f'We with {friend} best friends')
+        print(f"We with {friend} best friends")
 
