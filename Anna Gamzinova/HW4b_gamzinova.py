c9b1ba0bed4ae7333ab3c81b5168ebc29804f8c9
gamzinova <gamzinova@gmail.com>
2022-08-29 14:53:39 +0400

    [HW4] Task2 - multiplication and division

---
+++ Anna Gamzinova/HW4b_gamzinova.py
@@ -0,0 +1,12 @@
+# Multiplication and division 2 by 2 in two ways
+
+number = 2
+
+# Multiplication
+print(f'Two times two equals {number * 2}')
+print(f'Two times two bitwise equals {number << 1}')
+
+# Division
+print(f'Two divided by two equals {number // 2}')
+print(f'Two divided by two bitwise equals {number >> 1}')
+
