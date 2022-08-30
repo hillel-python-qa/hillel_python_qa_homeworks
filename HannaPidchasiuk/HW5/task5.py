5632d63ea8322f4bd5644c526f03887af92fdc3f
anutka <azaychenko1999@gmail.com>
2022-08-30 21:48:20 +0300

    added resolution for task5

--- HannaPidchasiuk/HW5/task5.py
+++ HannaPidchasiuk/HW5/task5.py
@@ -1,2 +1,18 @@
-# you have a text break the text(attached file) into sentences. As a result, you should get a list of sentences.
+# you have a text. break the text(attached file) into sentences. As a result, you should get a list of sentences.
+import re
 
+if __name__ == "__main__":
+    text = "The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that was " \
+           "launched into low Earth orbit in 1990 and remains in operation.... " \
+           "It was not the first space telescope, but it is one of the largest and most versatile, " \
+           "renowned both as a vital research tool and as a public relations boon for astronomy. " \
+           "The Hubble telescope is named after astronomer Edwin Hubble and is one of NASA's Great Observatories..... " \
+           "The Space Telescope Science Institute (STScI) selects Hubble's targets and processes the resulting data, " \
+           "while the Goddard Space Flight Center (GSFC) controls the spacecraft." \
+           "A commission headed by Lew Allen, director of the Jet Propulsion Laboratory, was established " \
+           "to determine how the error could have arisen. The Allen Commission found that a reflective null corrector, " \
+           "a testing device used to achieve a properly shaped non-spherical mirror, " \
+           "had been incorrectly assembled—one lens was out of position by 1.3 mm (0.051 in)."
+
+    for sentence in re.split(r'[.?!]\s', text):
+        print(sentence)
