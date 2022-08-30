a5751cfb8563027bbab7294c881264c97c44b271
anutka <azaychenko1999@gmail.com>
2022-08-30 20:05:36 +0300

    added resolution for task1

--- HannaPidchasiuk/HW5/task1.py
+++ HannaPidchasiuk/HW5/task1.py
@@ -1,3 +1,6 @@
 # there is a space-separated string with the names "john peter brian Morgan Adam Maria bart"
 # convert the string so that each name starts with a capital letter.
 
+if __name__=="__main__":
+    names = "john peter brian Morgan Adam Maria bart"
+    print(names.title())
