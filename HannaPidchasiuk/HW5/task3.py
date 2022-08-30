15dea20b8354cfc5a48fa4ee770a2a25bb5cedde
anutka <azaychenko1999@gmail.com>
2022-08-30 20:37:33 +0300

    added resolution for task3

--- HannaPidchasiuk/HW5/task3.py
+++ HannaPidchasiuk/HW5/task3.py
@@ -1,3 +1,12 @@
 # there is a string " name=Amanda=sssss&age=32&&salary=1500&currency=euro ".
-# Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: quro}
+# Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: euro}
 
+if __name__ == "__main__":
+    a = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
+    a_pairs = a.strip().replace("=sssss", "").split("&")
+    a_dict = {}
+    for pair in a_pairs:
+        if pair.find("=") > 0:
+            temp = pair.split("=")
+            a_dict.update({temp[0]: temp[1]})
+    print(a_dict)
