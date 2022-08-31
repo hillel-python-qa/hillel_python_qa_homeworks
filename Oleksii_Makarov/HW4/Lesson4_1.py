46af27e904cc97c0fef94ce90d76f47be694d47d
Neitore <alekseymkaale@gmail.com>
2022-08-31 21:34:43 +0300

    replacing for loop with for each

--- Oleksii_Makarov/HW4/Lesson4_1.py
+++ Oleksii_Makarov/HW4/Lesson4_1.py
@@ -1,10 +1,11 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 odd_index = []
 even_index = []
-for index in range(len(numbers)):
-    if index % 2:
-        odd_index.append((index, numbers[index]))
-    else:
-        even_index.append((index, numbers[index]))
-print(f'Odd index: {odd_index}\nEven index: {even_index}')
 
+for index, digit in enumerate(numbers):
+    if index % 2:
+        odd_index.append((index, digit))
+    else:
+        even_index.append((index, digit))
+
+print(f'Odd index: {odd_index}\nEven index: {even_index}')
