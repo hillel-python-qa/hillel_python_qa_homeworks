c3373dc67c7cff4f6fe6a515a3182e3911fb5155
VladyslavGurevych <gurik1989@ukr.net>
2022-09-01 18:14:35 +0100

    Add LOOP in Task2

--- Vladyslav_Gurevych/HW5/Task2.py
+++ Vladyslav_Gurevych/HW5/Task2.py
@@ -6,9 +6,7 @@
 list_of_friends = ["John", "Marta", "James", "Amanda", "Marianna"]
 name = "Names"
 
-print(f'{name.center(25, "*")}\n'
-      f'{list_of_friends[0]:>25}\n'
-      f'{list_of_friends[1]:>25}\n'
-      f'{list_of_friends[2]:>25}\n'
-      f'{list_of_friends[3]:>25}\n'
-      f'{list_of_friends[4]:>25}')
+print(f'{name.center(25, "*")}')
+
+for name in list_of_friends:
+      print(f'{name:>25}')
