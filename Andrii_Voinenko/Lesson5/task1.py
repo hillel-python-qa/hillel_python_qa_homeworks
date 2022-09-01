2a9d39d9bd20d52a7bb55720cafe3c3b307f0e04
andrii-voinenko <voynenko.andrey@gmail.com>
2022-09-01 16:26:21 +0300

    hw_first_solution

---
+++ Andrii_Voinenko/Lesson5/task1.py
@@ -0,0 +1,2 @@
+string = "john peter brian Morgan Adam Maria bart"
+print(string.title())
---
+++ Andrii_Voinenko/Lesson5/task3.py
@@ -0,0 +1,8 @@
+info = "name=Amanda=sssss&age=32&&salary=1500&currency=euro"
+update_info = info.split("=", maxsplit=2)
+update_info[2].split("=")
+print(update_info)
+
+my_dict = {}
+
+
