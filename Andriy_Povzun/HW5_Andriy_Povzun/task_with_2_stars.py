0ebe69286d887054579e64b65e5be42b55f299c4
AndriyPovzun <povzuun99@gmail.com>
2022-09-03 17:50:31 +0300

    delete_extra_function

--- Andriy_Povzun/HW5_Andriy_Povzun/task_with_2_stars.py
+++ Andriy_Povzun/HW5_Andriy_Povzun/task_with_2_stars.py
@@ -7,7 +7,7 @@ id_data = re.findall(r'id=\"[^id=\"block]\w+', html_data)
 list_links = re.findall(r'[a-z]+.com', html_data)
 text_of_the_links = re.findall(r'[A-Z]{1}[a-z]{2,}', html_data)
 
-id_data = ' '.join(id_data).replace('id=\"', '').rstrip().split(' ')
+id_data = ' '.join(id_data).replace('id=\"', '').split(' ')
 finish_list = []
 
 for i in range(len(id_data)):
