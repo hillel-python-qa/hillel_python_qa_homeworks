32fdae9011da96c6e11cd68c83566ef92c098a64
gamzinova <gamzinova@gmail.com>
2022-09-03 23:02:54 +0400

    [HW5] Task5 - in progress

---
+++ Anna Gamzinova/HW5e_gamzinova.py
@@ -0,0 +1,20 @@
+#  breaking the text into the sentences
+import re
+
+text = "The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that was launched into " \
+       "low Earth orbit in 1990 and remains in operation.... It was not the first space telescope, but it is one of " \
+       "the largest and most versatile, renowned both as a vital research tool and as a public relations boon for " \
+       "astronomy. The Hubble telescope is named after astronomer Edwin Hubble and is one of NASA's Great " \
+       "Observatories..... The Space Telescope Science Institute (STScI) selects Hubble's targets and processes the " \
+       "resulting data, while the Goddard Space Flight Center (GSFC) controls the spacecraft.A commission headed by " \
+       "Lew Allen, director of the Jet Propulsion Laboratory, was established to determine how the error could have " \
+       "arisen. The Allen Commission found that a reflective null corrector, a testing device used to achieve a " \
+       "properly shaped non-spherical mirror, had been incorrectly assembled—one lens was out of position by 1.3 mm (" \
+       "0.051 in). "
+# substitute the text by all non numeric '\.\s' to '.\n'
+text_sub = re.sub(r'(\D)(\.+\s*)', r'\g<1>.\n', text)
+# split text by '\n'
+text_split = re.split('\n', text_sub)
+
+for line in text_split:
+    print(line)
