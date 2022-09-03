0a8b4db652b82397253afc9d62a6912446aba67c
AndriyPovzun <povzuun99@gmail.com>
2022-09-03 19:37:00 +0300

    Full_change_resolution

--- Andriy_Povzun/HW5_Andriy_Povzun/Task3.py
+++ Andriy_Povzun/HW5_Andriy_Povzun/Task3.py
@@ -1,14 +1,17 @@
+import re
+
 string = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
 string = string.strip()
-string = string.replace('&&', '&')
-string = string.replace('&', '=')
-data_about_Amanda = string.split('=')
+data_about_Amanda = re.sub(r'=|&+', ' ', string)
+data_about_Amanda = data_about_Amanda.split(' ')
 data_about_Amanda.remove('sssss')
 
 dictionary = {}
 index = 0
 
-for times in range(int(len(data_about_Amanda) / 2)):
+for element in data_about_Amanda:
     dictionary[data_about_Amanda[index]] = data_about_Amanda[index + 1]
     index += 2
+    if index == len(data_about_Amanda):
+        break
 print(dictionary)
