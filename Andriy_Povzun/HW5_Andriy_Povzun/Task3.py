a97b55e61bcc1156a1a45f76165d1819635313f8
AndriyPovzun <povzuun99@gmail.com>
2022-09-04 17:05:49 +0300

    CreateBetter_re__ChangeVariableName

--- Andriy_Povzun/HW5_Andriy_Povzun/Task3.py
+++ Andriy_Povzun/HW5_Andriy_Povzun/Task3.py
@@ -2,16 +2,15 @@ import re
 
 string = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
 string = string.strip()
-data_about_Amanda = re.sub(r'=|&+', ' ', string)
-data_about_Amanda = data_about_Amanda.split(' ')
-data_about_Amanda.remove('sssss')
+data_about_amanda = re.sub(r'=s+&|=|&+', ' ', string)
+data_about_amanda = data_about_amanda.split(' ')
 
 dictionary = {}
 index = 0
 
-for element in data_about_Amanda:
-    dictionary[data_about_Amanda[index]] = data_about_Amanda[index + 1]
+for element in data_about_amanda:
+    dictionary[data_about_amanda[index]] = data_about_amanda[index + 1]
     index += 2
-    if index == len(data_about_Amanda):
+    if index == len(data_about_amanda):
         break
 print(dictionary)
