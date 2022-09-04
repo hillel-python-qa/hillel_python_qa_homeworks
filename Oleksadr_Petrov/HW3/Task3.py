47ee6a124a2010c4f5593a176ba69dee7da80978
alexander <petrovodessa1936@gmail.com>
2022-09-05 02:50:37 +0300

    Revert "add tasks 2-6 for hw4"
    This reverts commit 89c509a52be63791d4e449bc51d56434956504bc.

--- Oleksadr_Petrov/HW3/Task3.py
+++
@@ -1,6 +0,0 @@
-casino_blacklist = ["Minodora Áurea", "Itoro Alban", "Gabriella Emil", "Sigmund Govinda"]
-poker_blacklist = ["Minodora Áurea", "Itoro Alban", "Shaquille Rahma", "Oleksander Cenric"]
-alcohol_blacklist = ["Minodora Áurea", "Itoro Alban", "Shaquille Rahma", "Colm Eutychos"]
-
-result = set(casino_blacklist) & set(poker_blacklist) & set(alcohol_blacklist)
-print(result)
--- Oleksadr_Petrov/HW3/Task6.py
+++
@@ -1,6 +0,0 @@
-names = ["Minodora Áurea", "Itoro Alban", "Gabriella Emil", "Sigmund Govinda", "Minodora Áurea", "Itoro Alban"]
-result = []
-for name in names:
-    if name not in result:
-        result.append(name)
-print(result)
