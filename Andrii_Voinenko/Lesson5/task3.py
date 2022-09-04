25020a891b8b40702e102d5e8c00679fdee0f6e2
andrii-voinenko <voynenko.andrey@gmail.com>
2022-09-04 20:31:44 +0300

    HW5_resolution

--- Andrii_Voinenko/Lesson5/task3.py
+++ Andrii_Voinenko/Lesson5/task3.py
@@ -1,8 +1,27 @@
+import re
 info = "name=Amanda=sssss&age=32&&salary=1500&currency=euro"
-update_info = info.split("=", maxsplit=2)
-update_info[2].split("=")
-print(update_info)
 
-my_dict = {}
+update_info = re.split("\W", info)
+values_list = []
+
+for element in update_info:
+    if element == 'sssss':
+        update_info.remove(element)
+    elif element == '':
+        update_info.remove(element)
+
+
+for i, v in enumerate(update_info):
+    if i % 2 == 1:
+        values_list.append(v)
+
+
+del update_info[1::2]
+
+my_dict = dict(zip(update_info, values_list))
+print(my_dict)
+
+
+
 
 
