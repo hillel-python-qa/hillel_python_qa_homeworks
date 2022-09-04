958f89456a42eed94f0632f875c3a7a6a45ae400
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Air.local>
2022-09-04 11:14:19 +0300

    task2 completed

---
+++ Oleksii_Fedorchuk/HW5_Oleksii_Fedorchuk/task2.py
@@ -0,0 +1,10 @@
+friends = ["John", "Marta", "James", "Amanda", "Marianna"]
+title_string = ' Names '
+print(f"{title_string}".center(50, "*"))
+for names in friends:
+    print(f"{friends[0]}".rjust(27))
+    print(f"{friends[1]}".rjust(27))
+    print(f"{friends[2]}".rjust(27))
+    print(f"{friends[3]}".rjust(27))
+    print(f"{friends[4]}".rjust(27))
+    break
