e867afa990c6e5b0297119b2950fb35f36c999d1
AndriyPovzun <povzuun99@gmail.com>
2022-09-05 14:01:09 +0300

    change_resolve_task3

--- Andriy_Povzun/HW6_Andriy_Povzun/task3.py
+++ Andriy_Povzun/HW6_Andriy_Povzun/task3.py
@@ -1,16 +1,15 @@
 import re
+import string
 
 with open('text.txt', 'r') as file:
     text = file.read()
-upper_alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                  'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-lover_aplhabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                  'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+upper_alphabet = string.ascii_uppercase
+lover_aplhabet = string.ascii_lowercase
 
-i = 0
 for letter in upper_alphabet:
-    upper_letter = re.findall(upper_alphabet[i], text)
-    lover_letter = re.findall(lover_aplhabet[i], text)
-    print(f'Count of letter {upper_alphabet[i]} in upper case: {upper_letter.count(upper_alphabet[i])}\n'
-          f'Count of letter {lover_aplhabet[i]} in lover case: {lover_letter.count(lover_aplhabet[i])}\n')
-    i += 1
+    upper_letters = re.findall(letter, text)
+    print(f'Count of letter {letter} in upper case: {upper_letters.count(letter)}\n')
+
+for lover_letter in lover_aplhabet:
+    lover_letters = re.findall(lover_letter, text)
+    print(f'Count of letter {lover_letter} in lover case: {lover_letters.count(lover_letter)}\n')
