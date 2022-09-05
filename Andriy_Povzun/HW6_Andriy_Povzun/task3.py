9577730b6d9cba4cc341f6d92d548a12323238b1
AndriyPovzun <povzuun99@gmail.com>
2022-09-05 14:24:51 +0300

    change_resolve

--- Andriy_Povzun/HW6_Andriy_Povzun/task3.py
+++ Andriy_Povzun/HW6_Andriy_Povzun/task3.py
@@ -5,11 +5,3 @@ with open('text.txt', 'r') as file:
     text = file.read()
 upper_alphabet = string.ascii_uppercase
 lover_aplhabet = string.ascii_lowercase
-
-for letter in upper_alphabet:
-    upper_letters = re.findall(letter, text)
-    print(f'Count of letter {letter} in upper case: {upper_letters.count(letter)}\n')
-
-for lover_letter in lover_aplhabet:
-    lover_letters = re.findall(lover_letter, text)
-    print(f'Count of letter {lover_letter} in lover case: {lover_letters.count(lover_letter)}\n')
