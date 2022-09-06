e187659d274dbf3a1d59aadc285fc18f47bc6227
Main <natalybon1405@gmail.com>
2022-09-06 14:17:20 +0300

    task_3_fix

--- Nataly_Bondarenko/HW5/task3_string.py
+++ Nataly_Bondarenko/HW5/task3_string.py
@@ -1,9 +1,7 @@
 # Convert string to a dictionary
 string = "name=Amanda=sssss&age=32&&salary=1500&currency=euro"
-rep = string.replace('=sssss&', ' ')
-rep1 = rep.replace('&&', ' ')
-rep2 = rep1.replace('&', ' ')
+string_modification = string.replace('=sssss&', ' ').replace('&&', ' ').replace('&', ' ')
 
-dictionary = dict(subString.split("=") for subString in rep2.split(" "))
+dictionary = dict(subString.split("=") for subString in string_modification.split(" "))
 
 print(f'Dictionary :  {dictionary}')
