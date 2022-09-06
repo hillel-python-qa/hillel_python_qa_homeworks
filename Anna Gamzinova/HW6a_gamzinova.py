8723ea9274c59ca31fc86c071cd137a944ece00b
gamzinova <gamzinova@gmail.com>
2022-09-06 22:54:18 +0300

    [HW6] Task1 - creating a list of tuples and save to file - final

--- Anna Gamzinova/HW6a_gamzinova.py
+++ Anna Gamzinova/HW6a_gamzinova.py
@@ -4,6 +4,7 @@ from random import randint
 import os
 
 list_tuples = list()
+# a is left number, b is right number and c is operator
 for index in range(0, 100, 1):
     a = randint(0, 100)
     b = randint(0, 100)
@@ -17,3 +18,4 @@ file = "list_tuples.txt"
 with open(os.path.join(path, file), 'w') as fp:
     for tuple_line in list_tuples:
         fp.write(f'{tuple_line[0]} {tuple_line[1]} {tuple_line[2]}\n')
+
