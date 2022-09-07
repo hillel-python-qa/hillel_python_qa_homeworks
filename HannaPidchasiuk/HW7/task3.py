f50c32c566a237651be56e2b3109ec692e52a1c9
anutka <azaychenko1999@gmail.com>
2022-09-07 22:11:41 +0300

    added docs for task3

--- HannaPidchasiuk/HW7/task3.py
+++ HannaPidchasiuk/HW7/task3.py
@@ -1,15 +1,17 @@
 # Write a function is_prime that takes 1 argument - a number from 2 to 1000,
 # and returns True if it is a prime number, and False otherwise.
 
-def is_prime(number: int) -> bool:
-    if type(number) is str:
-        return False
-    elif 2 <= number <= 1000:
+def is_prime(number: float) -> bool:
+    """
+        Takes number and returns TRUE in case it is in range from 2 till 1000 (including).
+        Otherwise, returns FALSE
+    """
+    if 2 <= number <= 1000:
         return True
     else:
         return False
 
 
 if __name__ == "__main__":
-    print(is_prime(88))
+    print(is_prime(88.5))
 
