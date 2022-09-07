eef6403fb2a63af272b14e56c9391d7b5be37fb1
anutka <azaychenko1999@gmail.com>
2022-09-07 20:37:23 +0300

    added resolution for task2

--- HannaPidchasiuk/HW7/task2.py
+++ HannaPidchasiuk/HW7/task2.py
@@ -1,2 +1,17 @@
 # Write a function square that takes 1 argument, the side of the square, and returns 3 values
 # (using a tuple): the perimeter of the square, the area of the square, and the diagonal of the square.
+from math import sqrt
+
+
+def square(side) -> tuple:
+    square_perimeter = side * 4
+    square_area = pow(side, 2)
+    square_diagonal = round(side * sqrt(2), 3)
+    return square_perimeter, square_area, square_diagonal
+
+
+if __name__ == "__main__":
+    square_parameters = square(2)
+    print(f'Square perimeter: {square_parameters[0]}\n'
+          f'Square area: {square_parameters[1]}\n'
+          f'Square diagonal: {square_parameters[2]}')
