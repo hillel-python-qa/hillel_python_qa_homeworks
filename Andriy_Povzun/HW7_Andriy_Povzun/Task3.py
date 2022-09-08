53225371ce78fb90b1eff123a731a4a7e2e1dcb4
AndriyPovzun <povzuun99@gmail.com>
2022-09-08 18:19:16 +0300

    fix_task3

--- Andriy_Povzun/HW7_Andriy_Povzun/Task3.py
+++ Andriy_Povzun/HW7_Andriy_Povzun/Task3.py
@@ -1,4 +1,4 @@
-def is_prime(number: int):
+def is_prime(number: int) -> bool:
     if number < 2 or number > 1000:
         raise Exception('Function work with number in range from 2 to 1000')
     else:
@@ -6,7 +6,14 @@ def is_prime(number: int):
         for i in range(2, number - 1):
             if number % i == 0:
                 divided += 1
-        if divided == 0:
-            return True
-        else:
+                if divided > 0:
+                    break
+        if divided != 0:
             return False
+        else:
+            return True
+
+
+"""
+The function returns True if the number is prime, False otherwise
+"""
