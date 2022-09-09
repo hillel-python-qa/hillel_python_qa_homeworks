ca3f7091cebbdc93e4506610351ada5577a3a743
OleksiiAfanasenko <112640981+OleksiiAfanasenko@users.noreply.github.com>
2022-09-09 17:03:14 +0300

    1

--- Afanasenko_Oleksii/HW4_task3_afanasenko_oleksii.py
+++ Afanasenko_Oleksii/HW4_task3_afanasenko_oleksii.py
@@ -1,8 +1,8 @@
 friends = ["John", "Marta", "James"]
 enemies = ["John", "Johnatan", "Artur"]
 for friend in friends:
-    if friend not in enemies:
+    if friend not in enemies and friend != 'James':
         print(f'{friend} we are best friend')
     elif friend in enemies:
         print(f'{friend} we are not the friends anymore')
-    else: print('James we are best friend')
+
