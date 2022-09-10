ce25740b490c71b7a725a3c26ccf8267e68c200d
anutka <azaychenko1999@gmail.com>
2022-09-10 13:03:14 +0300

    remove space in the beggining of the line in case new line

--- HannaPidchasiuk/HW8/task1.py
+++ HannaPidchasiuk/HW8/task1.py
@@ -1,16 +1,18 @@
 # Implement your own print function. It should work on all operating systems.
-import os
 import sys
 
 
 def my_print(*args):
     temp_line = ''
     for item in args:
-        temp_line += str(item) + " "
+        if str(item).endswith("\n"):
+            temp_line += str(item)
+        else:
+            temp_line += str(item) + " "
     sys.stdout.write(temp_line)
     sys.stdout.write("\n")
 
 
 if __name__ == "__main__":
-    my_print(1, "sdfg", (3, 4), True, 2.333, [1, 2])
-    print(1, "sdfg", (3, 4), True, 2.333, [1, 2])
+    my_print(1, "sdfg\n", (3, 4), True, 2.333, [1, 2])
+    print(1, "sdfg\n", (3, 4), True, 2.333, [1, 2])
