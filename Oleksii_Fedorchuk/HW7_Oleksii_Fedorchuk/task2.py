d825bbc0318315c71e1196579023e5c148aa71d6
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Air.local>
2022-09-10 21:27:59 +0300

    task condition is added

--- Oleksii_Fedorchuk/HW7_Oleksii_Fedorchuk/task2.py
+++ Oleksii_Fedorchuk/HW7_Oleksii_Fedorchuk/task2.py
@@ -2,10 +2,10 @@
  perimeter of the square, the area of the square, and the diagonal of the square."""
 
 if __name__ == "__main__":
-    def calculating(side):
+    def square(side):
         perimeter = side * 4
         sqr = side ** 2
         diagonal = round((2 * side ** 2) ** .5, 2)
         result = (perimeter, sqr, diagonal)
         return result
-print(f"{calculating(5)}")
+print(f"{square(5)}")
