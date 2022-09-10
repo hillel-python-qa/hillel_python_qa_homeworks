3b04959661b0059731bace14a68b16be836cb3e9
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Air.local>
2022-09-10 21:29:00 +0300

    task condition is added

--- Oleksii_Fedorchuk/HW7_Oleksii_Fedorchuk/task1.py
+++ Oleksii_Fedorchuk/HW7_Oleksii_Fedorchuk/task1.py
@@ -1,3 +1,5 @@
+"""You have a file of unknown length. Write a function that will remove all numbers from each line of the file."""
+
 if __name__ == "__main__":
     def calc(a, b, c):
         if c == "+":
