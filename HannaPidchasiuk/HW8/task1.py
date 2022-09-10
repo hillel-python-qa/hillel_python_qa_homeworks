a9870cc10d5e7427f9c673440f589a4bd343ac4b
anutka <azaychenko1999@gmail.com>
2022-09-10 12:21:51 +0300

    resolution for task 1

--- HannaPidchasiuk/HW8/task1.py
+++ HannaPidchasiuk/HW8/task1.py
@@ -1,1 +1,16 @@
 # Implement your own print function. It should work on all operating systems.
+import os
+import sys
+
+
+def my_print(*args):
+    temp_line = ''
+    for item in args:
+        temp_line += str(item) + " "
+    sys.stdout.write(temp_line)
+    sys.stdout.write("\n")
+
+
+if __name__ == "__main__":
+    my_print(1, "sdfg", (3, 4), True, 2.333, [1, 2])
+    print(1, "sdfg", (3, 4), True, 2.333, [1, 2])
