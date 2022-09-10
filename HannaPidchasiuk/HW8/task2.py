23ccae9ed450ccbf3d06d07de6b0bc7869cec89f
anutka <azaychenko1999@gmail.com>
2022-09-10 12:58:59 +0300

    resolution for task 2

--- HannaPidchasiuk/HW8/task2.py
+++ HannaPidchasiuk/HW8/task2.py
@@ -1,1 +1,17 @@
 # Implement your realization of the function filter
+def my_filter(callback, sequence) -> list:
+    return [item for item in sequence if callback(item)]
+
+
+def check_is_true(item):
+    if item:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    test_sequence = (1, 2, 3, 0, 5, 7, 0)
+    print(my_filter(check_is_true, test_sequence))
+    print(list(filter(check_is_true, test_sequence)))
+
