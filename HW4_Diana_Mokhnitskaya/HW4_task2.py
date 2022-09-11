bb0c6ffdf64b5bb7974554d5046102d04019461e
dianamokhnitskaya <112774353+dianamokhnitskaya@users.noreply.github.com>
2022-09-11 18:55:24 +0300

    HW4_task_2  Multiplicate and divide by 2
    Multiplicate and divide by 2

---
+++ HW4_Diana_Mokhnitskaya/HW4_task2.py
@@ -0,0 +1,10 @@
+a = 2
+c = a * 2
+print("first variant of multiplication:" + str(c))
+a *= 2
+print("second variant of multiplication:" + str(a))
+
+d = c//2
+print("first variant of division:" + str(d))
+a //= 2
+print("second variant of division:" + str(a))
