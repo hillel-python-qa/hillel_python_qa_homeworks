2b5fceecc8eedf2642347fd37da2c21aabcf05e5
Neitore <alekseymkaale@gmail.com>
2022-09-12 18:19:33 +0300

    Homework 6

---
+++ Oleksii_Makarov/HW6/Lesson6_1.py
@@ -0,0 +1,15 @@
+from random import randint
+import os
+
+rand_tuples = list()
+
+for temp in range(0, 100):
+    right = randint(0, 100)
+    left = randint(0, 100)
+    operation = randint(1, 3)
+    rand_tuples.append((right, left, operation))
+
+os.makedirs('test/data')
+with open('./test/data/tuples_1.txt', 'w') as file:
+    for line in rand_tuples:
+        file.write(f'{line[0]} {line[1]} {line[2]}\n')
---
+++ Oleksii_Makarov/HW6/Lesson6_3.py
@@ -0,0 +1,16 @@
+import re
+
+with open('text.txt', 'r') as file:
+    text = file.read()
+
+letter_count = {}
+# This regex fucntion replaces almost every dash "-" with "Ђв" and I have no idea why
+# Hence telling it to also replace them. And it's working for some reason
+only_letters = re.sub(r'\d|\W|Ђ|в', '', text)
+for letter in sorted(only_letters, key=str.lower):
+    if letter in letter_count:
+        letter_count[letter] += 1
+    else:
+        letter_count[letter] = 1
+for key, value in letter_count.items():
+    print(f'{key}:{value}')
