91e1c1ae03a87e170811f005eaeb55604ffe97f3
anutka <azaychenko1999@gmail.com>
2022-09-13 20:36:59 +0300

    added resolution for task4

--- HannaPidchasiuk/HW9/task4.py
+++ HannaPidchasiuk/HW9/task4.py
@@ -1,1 +1,21 @@
 # Find all the numbers from 1-1000 that have a 3 in them
+def find_have_3_inside(min_elements: int, max_elements: int) -> list:
+    return [item for item in range(min_elements, max_elements) if str(item).count("3")]
+
+
+def my_gen():
+    counter = 1
+    while counter <= 1000:
+        if str(counter).count("3"):
+            yield counter
+        counter += 1
+
+
+if __name__ == "__main__":
+    print(find_have_3_inside(1, 1000))
+
+    gen = my_gen()
+    print(next(gen))
+    print(next(gen))
+    print(next(gen))
+    print(next(gen))
