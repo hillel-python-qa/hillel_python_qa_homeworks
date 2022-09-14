26b6433561fb0807ded3c58e4ef8ee8b8285dd7b
VladyslavGurevych <gurik1989@ukr.net>
2022-09-14 11:30:37 +0100

    HW9_Vladyslav_Gurevych

--- Vladyslav_Gurevych/HW9/Task1.py
+++ Vladyslav_Gurevych/HW9/Task1.py
@@ -1,19 +1,19 @@
 from typing import Callable
 
 
-def name_decorator(function_to_decorate: Callable):
+def decorator(function_to_decorate: Callable):
     def inner(a: int, b: int):
         return f'The {function_to_decorate.__name__} is {function_to_decorate(a, b)}'
 
     return inner
 
 
-@name_decorator
+@decorator
 def sum_of_digit(a: int, b: int) -> int:
     return a + b
 
 
-@name_decorator
+@decorator
 def sum_of_multi(a: int, b: int) -> int:
     return a * b
 
