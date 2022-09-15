4644bf683700c1d7b8d8ec5943c3ca3b149fe7cc
Neitore <alekseymkaale@gmail.com>
2022-09-15 15:05:06 +0300

    Removed comment

--- Oleksii_Makarov/HW6/Lesson6_3.py
+++ Oleksii_Makarov/HW6/Lesson6_3.py
@@ -4,8 +4,6 @@ with open('text.txt', 'r') as file:
     text = file.read()
 
 letter_count = {}
-# This regex fucntion replaces almost every dash "-" with "Ђв" and I have no idea why
-# Hence telling it to also replace them. And it's working for some reason
 only_letters = re.sub(r'[^a-zA-Z]', '', text)
 for letter in sorted(only_letters, key=str.lower):
     if letter in letter_count:
