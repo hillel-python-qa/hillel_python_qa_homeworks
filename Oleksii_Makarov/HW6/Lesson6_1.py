b6d254fefabf7dcfeeab77fd9bab9d63f8149b18
Neitore <alekseymkaale@gmail.com>
2022-09-15 15:18:21 +0300

    Updating with branch HW6

--- Oleksii_Makarov/HW6/Lesson6_1.py
+++ Oleksii_Makarov/HW6/Lesson6_1.py
@@ -8,8 +8,10 @@ for temp in range(0, 100):
     left = randint(0, 100)
     operation = randint(1, 3)
     rand_tuples.append((right, left, operation))
-
-os.makedirs('test/data')
+if os.path.exists('./test/data'):
+    pass
+else:
+    os.makedirs('./test/data')
 with open('./test/data/tuples_1.txt', 'w') as file:
     for line in rand_tuples:
         file.write(f'{line[0]} {line[1]} {line[2]}\n')
