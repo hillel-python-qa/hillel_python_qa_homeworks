e46082d98af27a79547c641a85cf72c9169ca713
Neitore <alekseymkaale@gmail.com>
2022-09-15 15:45:41 +0300

    Removing redundant logic. Adding parameter

--- Oleksii_Makarov/HW6/Lesson6_1.py
+++ Oleksii_Makarov/HW6/Lesson6_1.py
@@ -7,10 +7,7 @@ for temp in range(0, 100):
     left = randint(0, 100)
     operation = randint(1, 3)
     rand_tuples.append((right, left, operation))
-if os.path.exists('./test/data'):
-    pass
-else:
-    os.makedirs('./test/data')
+os.makedirs('./test/data', exist_ok=True)
 with open('./test/data/tuples_1.txt', 'w') as file:
     for line in rand_tuples:
         file.write(f'{line[0]} {line[1]} {line[2]}\n')
