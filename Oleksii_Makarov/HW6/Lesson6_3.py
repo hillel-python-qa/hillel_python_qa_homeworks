d705ca3d05ca5b37cb630893b21a78189e57f46f
Neitore <alekseymkaale@gmail.com>
2022-09-15 15:03:40 +0300

    Fixed regex code for non-letters

--- Oleksii_Makarov/HW6/Lesson6_3.py
+++ Oleksii_Makarov/HW6/Lesson6_3.py
@@ -6,7 +6,7 @@ with open('text.txt', 'r') as file:
 letter_count = {}
 # This regex fucntion replaces almost every dash "-" with "Ђв" and I have no idea why
 # Hence telling it to also replace them. And it's working for some reason
-only_letters = re.sub(r'\d|\W|Ђ|в', '', text)
+only_letters = re.sub(r'[^a-zA-Z]', '', text)
 for letter in sorted(only_letters, key=str.lower):
     if letter in letter_count:
         letter_count[letter] += 1
