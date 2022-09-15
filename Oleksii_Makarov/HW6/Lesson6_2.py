6883e080f7e01cf9078f574093f47ea00ada1823
Neitore <alekseymkaale@gmail.com>
2022-09-15 16:12:17 +0300

    Adding missing file

---
+++ Oleksii_Makarov/HW6/Lesson6_2.py
@@ -0,0 +1,17 @@
+with open('./test/data/tuples_1.txt', 'r') as file:
+    tuples = file.read().splitlines()
+for digits in tuples:
+    line = digits.split(' ')
+    left = int(line[0])
+    right = int(line[1])
+    operation = int(line[2])
+
+    if operation == 1:
+        math = left + right
+        print (f'{left}+{right}={math}')
+    elif operation == 2:
+        math = left - right
+        print(f'{left}-{right}={math}')
+    else:
+        math = left * right
+        print(f'{left}*{right}={math}')
