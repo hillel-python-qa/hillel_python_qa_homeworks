68ea132df2edfd4e6d2d62cb7bf6c9e1a33066f4
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Air.local>
2022-09-17 20:15:57 +0300

    task2 is added

---
+++ Oleksii_Fedorchuk/HW8_Oleksii_Fedorchuk/task2.py
@@ -0,0 +1,23 @@
+# print(list(filter(lambda item: item.startswith("B"), list_of_items))) - Example if filter
+from typing import Callable
+
+if __name__ == "__main__":
+    list_of_items = ["BMW", "Bugatti", "Mercedes", "Volkswagen"]
+
+
+    def filter_func(func: Callable, item: list | tuple) -> list:
+        filter_list = []
+        for items in item:
+            if func(items):
+                filter_list.append(items)
+        return filter_list
+
+
+    def checking(item: list) -> bool:
+        for elements in item:
+            if elements.startswith("M"):
+                return True
+            else:
+                return False
+
+print(filter_func(checking, list_of_items))
