852d66b85c55cab4d31f804aa5ebd974b2f8da50
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Air.local>
2022-09-17 22:40:37 +0300

    task1 is updated

--- Oleksii_Fedorchuk/HW8_Oleksii_Fedorchuk/task1.py
+++ Oleksii_Fedorchuk/HW8_Oleksii_Fedorchuk/task1.py
@@ -1,9 +1,8 @@
 import sys
 
-
-def printing_func(*args):
-    for arg in args:
-        sys.stdout.write(str(arg) + "")
-
+if __name__ == "__main__":
+    def printing_func(*args):
+        for arg in args:
+            sys.stdout.write(str(arg) + "")
 
 printing_func("234")
