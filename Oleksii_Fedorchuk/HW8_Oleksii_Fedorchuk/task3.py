45e19d36db4e2fbce3738db7237df6e20db44924
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Air.local>
2022-09-17 21:07:31 +0300

    task3 is added

---
+++ Oleksii_Fedorchuk/HW8_Oleksii_Fedorchuk/task3.py
@@ -0,0 +1,11 @@
+list_of_numbers = [1, 2, 3, 4]
+new_list_of_numbers = []
+
+
+def map_func(number: int) -> int:
+    for numbers in list_of_numbers:
+        new_list_of_numbers.append(numbers + 1)
+    print(new_list_of_numbers)
+
+
+print(map_func(new_list_of_numbers))
