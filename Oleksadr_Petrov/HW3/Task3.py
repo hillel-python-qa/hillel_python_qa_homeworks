6a272459808ac29acc3df89ee398736a4eab0a99
alexander <petrovodessa1936@gmail.com>
2022-09-19 02:04:29 +0300

    HW3 restore

---
+++ Oleksadr_Petrov/HW3/Task3.py
@@ -0,0 +1,4 @@
+omnivores = ["Minodora Áurea", "Itoro Alban", "Gabriella Emil", "Sigmund Govinda"]
+vegetarians = ["Ivan Ivanov", "Oleg Petrov", "Sergey Smirnov", "Egor Letov", "Vitaliy Sidorov"]
+vegetables_eaters = omnivores + vegetarians
+print(vegetables_eaters)
