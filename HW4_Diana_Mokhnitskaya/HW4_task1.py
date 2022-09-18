24dd287dd8a753ae1a200d7369f99156160d6b2c
dianamokhnitskaya <112774353+dianamokhnitskaya@users.noreply.github.com>
2022-09-18 20:35:46 +0300

    Update HW4_task1.py

--- HW4_Diana_Mokhnitskaya/HW4_task1.py
+++ HW4_Diana_Mokhnitskaya/HW4_task1.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+umbers = [1, 2, 3, 4, 5, 6, 7, 8]
 list_with_index_and_value = tuple(enumerate(numbers))
-
-for even in range(0, 7, 2):
-    print("Even index:" + str(list_with_index_and_value[even]))
-for odd in range(1, 8, 2):
-    print("Odd index:" + str(list_with_index_and_value[odd]))
+for number in range(0, len(numbers)):
+    if number % 2 == 0:
+        print("Even index:" + str(list_with_index_and_value[number]))
+    else:
+        print("Odd index:" + str(list_with_index_and_value[number]))
