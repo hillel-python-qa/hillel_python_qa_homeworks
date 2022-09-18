b50ca9e2a2638ae78a24ccf38d8d83fbe035bfff
dianamokhnitskaya <112774353+dianamokhnitskaya@users.noreply.github.com>
2022-09-19 00:15:44 +0300

    Update HW4_task2.py

--- HW4_Diana_Mokhnitskaya/HW4_task2.py
+++ HW4_Diana_Mokhnitskaya/HW4_task2.py
@@ -1,10 +1,10 @@
 a = 2
 c = a * 2
 print("first variant of multiplication:" + str(c))
-a *= 2
-print("second variant of multiplication:" + str(a))
+c_bin = bin(a << 1)
+print("second variant of multiplication:", int(c_bin, 2))
 
 d = c//2
 print("first variant of division:" + str(d))
-a //= 2
-print("second variant of division:" + str(a))
+d_bin = bin(c >> 1)
+print("second variant of division:", int(d_bin, 2))
