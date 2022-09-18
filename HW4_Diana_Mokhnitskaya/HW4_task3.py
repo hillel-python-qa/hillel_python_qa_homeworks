dcbcd2a994099ed52d22d286f142040999d1c664
dianamokhnitskaya <112774353+dianamokhnitskaya@users.noreply.github.com>
2022-09-19 00:40:10 +0300

    Update HW4_task3.py

--- HW4_Diana_Mokhnitskaya/HW4_task3.py
+++ HW4_Diana_Mokhnitskaya/HW4_task3.py
@@ -2,16 +2,17 @@ friends_list = ["John", "Marta", "James"]
 enemies_list = ["John", "Johnatan", "Artur"]
 for friend in friends_list:
     if friend == "James":
-        break
-    if friend not in enemies_list:
+        print("James is my best friend")
+    elif friend not in enemies_list:
         print(f"{friend} we are the best friends"),
     else:
         print(f"{friend} we are not the friends anymore")
-print("James is my best friend")
+
 for enemy in enemies_list:
     if enemy not in friends_list:
         print(f"{enemy} we are not the friends anymore"),
+    elif enemy == "James":
+        print("James is my best friend")
     else:
         print(f"{enemy} we are the best friends")
-    if enemy == "James":
-        print("James is my best friend")
+
