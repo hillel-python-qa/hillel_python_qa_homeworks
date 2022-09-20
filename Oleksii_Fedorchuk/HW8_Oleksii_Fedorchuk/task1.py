0a15fef1e96ef6e9cba1a87f08e44a482ecdef9a
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Air.local>
2022-09-20 23:22:35 +0300

    tasks are updated __name__=='__main__' strings are deleted

--- Oleksii_Fedorchuk/HW8_Oleksii_Fedorchuk/task1.py
+++ Oleksii_Fedorchuk/HW8_Oleksii_Fedorchuk/task1.py
@@ -1,8 +1,9 @@
 import sys
 
-if __name__ == "__main__":
-    def printing_func(*args):
-        for arg in args:
-            sys.stdout.write(str(arg) + "")
+
+def printing_func(*args):
+    for arg in args:
+        sys.stdout.write(str(arg) + "")
+
 
 printing_func("234")
