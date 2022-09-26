93d4d370316f6cfebc882d556d266a6fae97a49b
AndriyPovzun <povzuun99@gmail.com>
2022-09-26 10:47:41 +0300

    Fix_Task1

--- Andriy_Povzun/HW12/Task1.py
+++ Andriy_Povzun/HW12/Task1.py
@@ -1,40 +1,99 @@
-class Apple:
-    @staticmethod
-    def general_information():
-        with open('Apple_wiki.txt', 'r') as gen_info:
-            information = gen_info.read()
-        return information
+from typing import Union
 
-    @staticmethod
-    def quantity_of_employees():
-        return 80000
 
-    @staticmethod
-    def founders_company():
-        return ['Steve Jobs', 'Steve Wozniak', 'Ronald Wayne']
+class Company:
+    def __init__(self, company_name: str, number_employees: int, company_products: list, country: str):
+        self.__company_name = company_name
+        self.__number_employees = number_employees
+        self.__company_products = company_products
+        self.__country = country
 
-    @staticmethod
-    def revenue():
-        return '365.82 billion $ USD on 2021 year'
+    @property
+    def company_name(self):
+        return self.__company_name
 
-    @staticmethod
-    def first_company_product():
-        return 'Apple I, it\'s first company product, demonstrated in April 1976 '
+    @company_name.setter
+    def company_name(self, new_company_name: str):
+        self.__company_name = new_company_name
 
-    @staticmethod
-    def company_value():
-        return 'Performance indicators As of October 31, 2021, the company\'s value was $2.41 trillion.' \
-               ' As of December 8, 2021, the company\'s market value reached $2.86 trillion.'
+    """
+    The method is used to change the company name
+    """
 
-    @staticmethod
-    def number_of_stores():
-        return 520
+    @company_name.deleter
+    def company_name(self):
+        pass
 
-    @staticmethod
-    def best_selled_product():
-        return 'The best-selling product in the history of the company is not an iPhone, but an adapter' \
-               ' cable from 3.5 mm to lighting'
+    """
+    Deleter written so that users do not break the design of the class
+    """
+
+    @property
+    def number_employees(self):
+        return self.__number_employees
+
+    def add_new_employees(self, quantity_new_employees: int):
+        self.__number_employees = self.__number_employees + quantity_new_employees
+
+    @number_employees.deleter
+    def number_employees(self):
+        pass
+
+    """
+    Deleter written so that users do not break the design of the class
+    """
+
+    @property
+    def company_products(self):
+        return self.__company_products
+
+    def add_new_products(self, *args: str):
+        self.__company_products.append(*args)
+
+    """
+    The method is used to add new company products
+    """
+
+    def withdraw_from_production(self, *args: str):
+        for arg in args:
+            if arg not in self.__company_products:
+                raise TypeError(f'Тo such element exists: {arg}')
+            else:
+                self.__company_products.remove(arg)
+
+    """
+    The method is used to withdraw products remove the product/products from production
+    """
+
+    @company_products.deleter
+    def company_products(self):
+        pass
+
+    """
+    Deleter written so that users do not break the design of the class
+    """
+
+    @property
+    def country(self):
+        return self.__country
+
+    @country.setter
+    def country(self, new_country: str):
+        self.__country = new_country
+
+    @country.deleter
+    def country(self):
+        pass
+
+    """
+    Deleter written so that users do not break the design of the class
+    """
 
 
 if __name__ == '__main__':
-    pass
+    apple = Company('Apple inc.', 80000, ['MacBook', 'iPhone', 'AirPods'], 'USA')
+    print(apple.company_products)
+    apple.add_new_products('iPad, iPod')
+    print(apple.company_products)
+    apple.withdraw_from_production('MacBook', 'iPhone')
+    print(apple.company_products)
