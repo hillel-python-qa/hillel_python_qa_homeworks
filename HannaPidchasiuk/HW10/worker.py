2f7287e34617c22761c41149d22ad5f2623dce4c
anutka <azaychenko1999@gmail.com>
2022-09-26 17:46:28 +0300

    extended worker with some enums

--- HannaPidchasiuk/HW10/worker.py
+++ HannaPidchasiuk/HW10/worker.py
@@ -1,10 +1,18 @@
+from grade_enum import Grade
+from department_enum import Department
+from type_of_work_enum import TypeOfWork
+
+
 class Worker:
 
-    def __init__(self, first_name: str, last_name: str, salary: float, department: str):
+    def __init__(self, first_name: str, last_name: str, salary: float, department: Department, grade: Grade,
+                 type_of_work: TypeOfWork):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__salary = salary
         self.__department = department
+        self.__grade = grade
+        self.__type_of_work = type_of_work
 
     @property
     def first_name(self):
@@ -59,12 +67,42 @@ class Worker:
         return self.__department
 
     @department.setter
-    def department(self, new_value: str):
+    def department(self, new_value: Department):
         """
             Set new department property of the worker.
             Takes only 1 argument: new_value.
         """
         self.__department = new_value
+
+    @property
+    def grade(self):
+        """
+            Returns grade property of the worker.
+        """
+        return self.__grade
+
+    @grade.setter
+    def grade(self, new_value: Department):
+        """
+            Set new grade property of the worker.
+            Takes only 1 argument: new_value.
+        """
+        self.__grade = new_value
+
+    @property
+    def type_of_work(self):
+        """
+            Returns type of work for worker.
+        """
+        return self.__type_of_work
+
+    @type_of_work.setter
+    def type_of_work(self, new_value: TypeOfWork):
+        """
+            Set new type of work for the worker.
+            Takes only 1 argument: new_value.
+        """
+        self.__type_of_work = new_value
 
     def count_overtimes(self, overtime_hours: int, average_hours: float) -> float:
         """
@@ -79,5 +117,7 @@ class Worker:
 
 
 if __name__ == "__main__":
-    worker = Worker("sd", "sd", 1300.50, "qa")
-    print(worker.count_overtimes(11, 77))
+    worker = Worker("name", "last_name", 12333, Department.QA_MANUAL, Grade.JUNIOR, TypeOfWork.OFFICE)
+    worker.type_of_work = TypeOfWork.REMOTE
+    worker.last_name = "kjf"
+    print(worker.last_name, worker.type_of_work)
