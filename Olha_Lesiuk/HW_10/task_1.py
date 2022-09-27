dcc13457208d70a032ed9f5aab7d36de8a254e9e
OlhaLesiuk <vazhyk@gmail.com>
2022-09-27 20:13:27 +0300

    Fixed issues in the HW_10

--- Olha_Lesiuk/HW_10/task_1.py
+++ Olha_Lesiuk/HW_10/task_1.py
@@ -1,29 +1,30 @@
-class Toyota:
-    # Class Variable
-    company = 'Toyota'
+class Company:
 
     # The init method or constructor
-    def __init__(self, organisational_structure: dict, top_industries: list):
+    def __init__(self, company_name: str, organisational_structure: dict, top_industries: list):
         # Instance Variable
+        self.__company_name = company_name
         self.__organisational_structure = organisational_structure
         self.__top_industries = top_industries
+
+    @property
+    def company_name(self):
+        return self.__company_name
 
     @property
     def get_organisational_structure(self):
         return self.__organisational_structure
 
+    @property
     def top_industries(self):
         return self.__top_industries
 
 
-ToyotaOrgStructure = {'Toyota Motor Corp, (TMC)': 'Japan', 'Toyota Motor Credit Corp': 'Japan',
-                      'Toyota Motor Sales USA': 'Torrance, Calif',
-                      'Toyota Motor Engineering and Manufacturing': 'Erlanger, Ky',
-                      'Toyota Motor North America': 'New York', 'Toyota Financial Services': 'USA'}
-
-ToyotaTopIndustries = ['Materials Handling Equipment', 'Automobiles', 'Textile Machinery']
-
 if __name__ == '__main__':
-    print(Toyota.company)
-    print(ToyotaOrgStructure)
-    print(ToyotaTopIndustries)
+    Company.organisational_structure = {'Toyota Motor Corp, (TMC)': 'Japan', 'Toyota Motor Credit Corp': 'Japan',
+                                        'Toyota Motor Sales USA': 'Torrance, Calif',
+                                        'Toyota Motor Engineering and Manufacturing': 'Erlanger, Ky',
+                                        'Toyota Motor North America': 'New York', 'Toyota Financial Services': 'USA'}
+    Company.industries = ['Materials Handling Equipment', 'Automobiles', 'Textile Machinery']
+
+    print(Company.organisational_structure, Company.industries)
--- Olha_Lesiuk/HW_10/task_2.py
+++ Olha_Lesiuk/HW_10/task_2.py
@@ -21,12 +21,28 @@ class Employees:
         """
         return self.__name
 
+    @name.setter
+    def name(self, name: str):
+        """
+        The method is used in case of changing to the new name
+        """
+        self.__name = name
+
     @property
     def age(self):
         """
         The method is used to obtain worker's age
         """
         return self.__age
+
+    @age.setter
+    def age(self, age: int):
+        """
+        The method is used in case of reaching a certain age
+        """
+        if age < 18:
+            raise ValueError("Sorry, your age is below eligibility criteria for this position")
+        self.__age = age
 
     @property
     def gender(self):
@@ -35,12 +51,26 @@ class Employees:
         """
         return self.__gender
 
+    @gender.setter
+    def gender(self, gender: str):
+        """
+        The method is used in case of changing gender
+        """
+        self.__gender = gender
+
     @property
     def position(self):
         """
         The method is used to obtain worker's position
         """
         return self.__position
+
+    @position.setter
+    def position(self, position: str):
+        """
+        The method is used in case of promotion to the higher position
+        """
+        self.__position = position
 
     @property
     def company(self):
@@ -49,6 +79,13 @@ class Employees:
         """
         return self.__company
 
+    @company.setter
+    def company(self, company: str):
+        """
+        The method is used in case of changing to the new company
+        """
+        self.__company = company
+
     @property
     def salary(self):
         """
@@ -56,7 +93,15 @@ class Employees:
         """
         return self.__salary
 
+    @salary.setter
+    def salary(self, salary: int):
+        """
+        The method is used in case of changing a salary
+        """
+        self.__salary = salary
+
 
 if __name__ == '__main__':
     john = Employees(name='John Wick', age=35, gender='male', position='hitman', company='Unknown', salary=4000)
+    john.age = 15
     print(john.name, john.age, john.gender, john.position, john.company, john.salary)
