9dae7b95b89cfd04a0399e92641bbe457bef9b21
Main <natalybon1405@gmail.com>
2022-09-30 01:10:52 +0300

    HW10_Nataly

---
+++ Nataly_Bondarenko/HW10/task1_class_company.py
@@ -0,0 +1,124 @@
+class Company:
+    """
+    The class Company constructor accepts the following information:
+    company_name, company_products, number_of_employees and company_locations
+    """
+
+    def __init__(self, company_name: str, company_products: list, number_of_employees: int, company_locations: list):
+        self.__company_name = company_name
+        self.__company_products = company_products
+        self.__number_of_employees = number_of_employees
+        self.__company_locations = company_locations
+
+    @property
+    def company_name(self):
+        """
+        Method  getting the company's name
+        """
+        return self.__company_name.capitalize()
+
+    @company_name.setter
+    def company_name(self, new_company_name: str):
+        """
+        Method changing the company's name
+        """
+        if len(new_company_name):
+            self.__company_name = new_company_name
+        else:
+            print('Empty value is not supported for company name')
+
+    @company_name.deleter
+    def company_name(self):
+        """
+        method will protect code against class design break
+        """
+        pass
+
+    @property
+    def company_products(self):
+        """
+        Method getting list of the company's products
+        """
+        return self.__company_products
+
+    @company_products.setter
+    def company_products(self, new_company_product: str):
+        """
+        Method to update the company products
+        """
+        if new_company_product in self.__company_products:
+            print(f"{new_company_product} is already in the company products list")
+        elif not len(new_company_product):
+            print('Empty value is not supported for company product')
+        else:
+            self.__company_products.append(new_company_product)
+
+    def product_to_remove(self, product_to_remove: str):
+        """
+        Method to remove the company products
+        """
+        if product_to_remove in self.__company_products:
+            return self.__company_products.remove(product_to_remove)
+        else:
+            print(f'{product_to_remove} can not be found in company product list')
+
+    @property
+    def number_of_employees(self):
+        """
+        Method getting the number of the company's employees
+        """
+        return self.__number_of_employees
+
+    @number_of_employees.setter
+    def number_of_employees(self, new_employees: str):
+        """
+        Method to update the number of the company's employees
+        """
+        if new_employees < 1:
+            print(f'{new_employees} unsupported number of employees')
+        else:
+            self.__number_of_employees += new_employees
+
+    @property
+    def company_locations(self):
+        """
+        Method getting the list of the company locations
+        """
+        return self.__company_locations
+
+    @company_locations.setter
+    def company_locations(self, new_company_location: str):
+        """
+        Method to update the list of the company locations
+        """
+        if new_company_location in self.__company_locations:
+            print(f"{new_company_location} is already in the company location list")
+        elif not len(new_company_location):
+            print('Empty value is not supported for company location')
+        else:
+            self.__company_locations.append(new_company_location)
+
+
+if __name__ == '__main__':
+    my_company_Dell = Company('Dell', ['Computer', 'Mouse', 'Keyboard'], 150000,
+                              ['Toronto', 'Bangkok', 'Kiev'])
+
+    my_company_Dell.company_name = 'samsung'
+    print(my_company_Dell.company_name)
+    del my_company_Dell.company_name
+    print(my_company_Dell.company_name)
+
+    my_company_Dell.company_products = 'Camera'
+    print(my_company_Dell.company_products)
+    my_company_Dell.product_to_remove('Camera')
+    print(my_company_Dell.company_products)
+
+    my_company_Dell.number_of_employees = 100
+    my_company_Dell.number_of_employees = 0
+    print(my_company_Dell.number_of_employees)
+
+    my_company_Dell.company_locations = 'Jerusalem'
+    my_company_Dell.company_locations = 'Kiev'
+    print(my_company_Dell.company_locations)
+    my_company_Dell.company_locations = ''
+    print(my_company_Dell.company_locations)
---
+++ Nataly_Bondarenko/HW10/task2_class_worker.py
@@ -0,0 +1,179 @@
+class Worker:
+    def __init__(self, first_name: str, last_name: str, age: int, position: str, salary: float):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__age = age
+        self.__position = position
+        self.__salary = salary
+
+    @property
+    def first_name(self):
+        """
+        Method  getting the worker's name
+        """
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, new_first_name: str):
+        """
+        method allows to change the name of the worker
+        """
+        if len(new_first_name):
+            self.__first_name = new_first_name.capitalize()
+        else:
+            print('Empty value is not supported for worker name')
+
+    @first_name.deleter
+    def first_name(self):
+        """
+        method will protect code against class design break
+        """
+        pass
+
+    @property
+    def last_name(self):
+        """
+        Method  getting the worker's last name
+        """
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, new_last_name: str):
+        if self.__last_name == new_last_name:
+            print(f'{new_last_name} - this last name already exists for this worker')
+        elif not len(new_last_name):
+            print('Empty value is not supported for worker last name')
+        else:
+            self.__last_name = new_last_name.capitalize()
+
+    @last_name.deleter
+    def last_name(self):
+        """
+        method will protect code against class design break
+        """
+        pass
+
+    @property
+    def age(self):
+        """
+        Method  getting the worker's age
+        """
+        return self.__age
+
+    @age.setter
+    def age(self, age_verification):
+        """
+        Method verified that worker's age is more than 18 years old
+        """
+        if age_verification < 18:
+            print(f'Sorry, people under 18 years old can not works in our company')
+        else:
+            print('We are glad that you are part of our company')
+
+    def grow_up(self):
+        self.__age += 1
+
+    @age.deleter
+    def age(self):
+        """
+        method will protect code against class design break
+        """
+        pass
+
+    @property
+    def position(self):
+        """
+        Method  getting the worker's position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, new_position: str):
+        """
+        Method to update the worker's position in the company
+        """
+        if self.__position == new_position:
+            print('this position is identical to the one in which the employee is already works')
+        elif not len(new_position):
+            print('Empty value is not supported for determine the position')
+        else:
+            self.__position = new_position
+
+    @position.deleter
+    def position(self):
+        """
+        method will protect code against class design break
+        """
+        pass
+
+    @property
+    def salary(self):
+        """
+        Method  getting the worker's salary
+        """
+        return self.__salary
+
+    @salary.setter
+    def salary(self, salary_increase: int):
+        """
+        Method to increase the worker's salary
+        """
+        if salary_increase < 1:
+            print(f'{salary_increase} is unsupported value for salary_increase')
+        elif salary_increase <= self.__salary:
+            print('salary cannot be lower than current salary')
+        else:
+            self.__salary = salary_increase
+
+    def worker_bonus(self):
+        """
+        Method to count the  worker's bonus based on worker's age
+        """
+        if self.__age < 30:
+            return self.__salary + 1000
+        elif 30 < self.__age < 40:
+            return self.__salary + 2000
+        else:
+            return self.__salary + 5000
+
+    @staticmethod
+    def annual_bonus():
+        """
+        Method to count the  annual bonus for each employee
+        """
+        return 274 * 50 + 100
+
+
+if __name__ == '__main__':
+    worker = Worker('Benedict', 'Eggs', 37, 'Technical support', 12000)
+
+    worker.first_name = 'lucas'
+    print(worker.first_name)
+    worker.first_name = ''
+    del worker.first_name
+    print(worker.first_name)
+
+    worker.last_name = 'Eggs'
+    worker.last_name = 'brauni'
+    print(worker.last_name)
+
+    worker.age = 5
+    worker.age = 22
+    print(worker.age)
+    worker.grow_up()
+    print(worker.age)
+
+    worker.position = 'Technical support'
+    worker.position = ''
+    print(worker.position)
+    worker.position = 'Team Lead Technical Support'
+    del worker.position
+    print(worker.position)
+
+    worker.salary = 1
+    print(worker.salary)
+    worker.salary = 15000
+    print(worker.salary)
+
+    print(worker.annual_bonus())
+    print(worker.worker_bonus())
