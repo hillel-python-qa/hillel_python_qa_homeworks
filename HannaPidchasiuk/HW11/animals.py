385b49f8b09d98818112836916fd54d15042736a
anutka <azaychenko1999@gmail.com>
2022-09-29 21:36:10 +0300

    added some methods

--- HannaPidchasiuk/HW11/animals.py
+++ HannaPidchasiuk/HW11/animals.py
@@ -2,9 +2,8 @@ from HannaPidchasiuk.HW11.area_of_living_enum import AreaOfLiving
 
 
 class Animals:
-    def __init__(self, species: str, subspecies: str, number_of_subspecies: int, area_of_living: AreaOfLiving):
+    def __init__(self, species: str, number_of_subspecies: int, area_of_living: AreaOfLiving):
         self.__species = species
-        self.__subspecies = subspecies
         self.__number_of_subspecies = number_of_subspecies
         self.__area_of_living = area_of_living
 
@@ -17,18 +16,7 @@ class Animals:
         if new_value:
             self.__species = new_value
         else:
-            raise ValueError("New value can't be empty!")
-
-    @property
-    def subspecies(self):
-        return self.__subspecies
-
-    @subspecies.setter
-    def subspecies(self, new_value: str):
-        if new_value:
-            self.__subspecies = new_value
-        else:
-            raise ValueError("New value can't be empty!")
+            raise ValueError("species can't be empty!")
 
     @property
     def number_of_subspecies(self):
@@ -39,7 +27,7 @@ class Animals:
         if not new_value < 0:
             self.__number_of_subspecies = new_value
         else:
-            raise ValueError("New value can't be less than zero!")
+            raise ValueError("number_of_subspecies can't be less than zero!")
 
     @property
     def area_of_living(self):
@@ -48,3 +36,12 @@ class Animals:
     @area_of_living.setter
     def area_of_living(self, new_value: AreaOfLiving):
         self.__area_of_living = new_value
+
+    def subspecies_die(self):
+        if self.__number_of_subspecies >= 1:
+            return self.__number_of_subspecies - 1
+        else:
+            print("All subspecies already dead.")
+
+    def found_new_subspecies(self):
+        return self.__number_of_subspecies + 1
