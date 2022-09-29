2250349838b38c269c20a4e03deee8fbf86b7868
anutka <azaychenko1999@gmail.com>
2022-09-29 22:41:48 +0300

    extended with method

--- HannaPidchasiuk/HW11/mamals.py
+++ HannaPidchasiuk/HW11/mamals.py
@@ -24,3 +24,12 @@ class Mammals(Animals):
             self.__number_of_paws = new_value
         else:
             raise ValueError("number_of_paws can't be less than zero!")
+
+    def should_create_children(self):
+        """
+            If number of subspecies is low - need to create children.
+        """
+        if self.number_of_subspecies < 10:
+            return True
+        else:
+            return False
