a4e9039a1ab766ee0a278b56b4ccf6dddec66f81
alexander <petrovodessa1936@gmail.com>
2022-10-03 02:34:51 +0300

    HW10 fixes

--- Oleksadr_Petrov/HW10/Apple.py
+++ Oleksadr_Petrov/HW10/Apple.py
@@ -1,42 +1,54 @@
 class Apple:
 
-    def __init__(self, name: str, employees: list, products: list, customers: list):
-        self.name = name
-        self.employees = employees
-        self.products = products
-        self.customers = customers
+    def __init__(self, name: str, employees: list, products: list, shops: list):
+        self.__name = name
+        self.__employees = employees
+        self.__products = products
+        self.__shops = shops
 
     @property
-    def employees(self):
-        return self.employees
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        """
+        Name cannot be empty
+        """
+        if name == '':
+            raise Exception('No empty name!')
+        else:
+            self.__name = name
 
     @property
-    def products(self):
-        return self.products
+    def employees(self) -> list:
+        return self.__employees
 
     @property
-    def employees(self):
-        return self.customers
+    def products(self) -> list:
+        return self.__products
+
+    @property
+    def shops(self):
+        return self.__shops
 
     def add_employee(self, employee):
-        self.employees().append(employee)
+        self.__employees().append(employee)
 
     def remove_employee(self, employee):
-        self.employees().remove(employee)
+        self.__employees().remove(employee)
 
     def add_product(self, product):
-        self.products().append(product)
+        self.__products().append(product)
 
     def remove_product(self, product):
-        self.products().remove(product)
+        self.__products().remove(product)
 
-    def add_customer(self, customer):
-        self.customers().append(customer)
+    def add_shop(self, shop):
+        self.__shops().append(shop)
 
-    def remove_customer(self, customer):
-        self.customers().remove(customer)
+    def remove_shop(self, shop):
+        self.__shops().remove(shop)
 
-
-
-
-
+    def get_employees_count(self) -> int:
+        return len(self.__employees)
