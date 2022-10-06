45869005bb1afba87706fff5a32c4b472536d359
andrii-voinenko <voynenko.andrey@gmail.com>
2022-10-06 19:54:45 +0300

    fixe_founded_issues

--- Andrii_Voinenko/Lesson13/animal.py
+++ Andrii_Voinenko/Lesson13/animal.py
@@ -1,28 +1,21 @@
-from abc import ABC, abstractmethod
-
-
-class Animals(ABC):
+class Animals:
     def __init__(self, name: str, gender: str, health: str):
-        self.__name = name
-        self.__gender = gender
-        self.__health = health
-        self.__feeding = None
+        self._name = name
+        self._gender = gender
+        self._health = health
+        self._feeding = None
 
-        if self.__health not in ['bad', 'normal', 'good']:
+        if self._health not in ['bad', 'normal', 'good']:
             raise TypeError('Set correct value for health: bad, normal or good')
 
     def creature_death(self):
-        if self.__health == 'bad':
+        if self._health == 'bad':
             return False
         else:
             return True
 
     def creature_mating(self):
-        if self.__health == 'bad':
+        if self._health == 'bad':
             print('Health of the creature is too bad for mating')
-            self.__feeding = True
-            return self.__feeding
-
-
-
-
+            self._feeding = True
+            return self._feeding
--- Andrii_Voinenko/Lesson13/mammals.py
+++ Andrii_Voinenko/Lesson13/mammals.py
@@ -1,19 +1,23 @@
+from abc import ABC, abstractmethod
 from animal import Animals
 
 
-class Mammals(Animals):
+class Mammals(Animals, ABC):
     def __init__(self, name: str, gender: str, health: str, claws: bool, ration: str):
         super().__init__(name, gender, health)
         self.__claws = claws
         self.__ration = ration
 
     def feed_baby(self):
-        if self.__feeding:
-            print(f'The {self.__name} can feed its baby')
+        if self._feeding:
+            print(f'The {self._name} can feed its baby')
 
     def creature_ration(self):
         if self.__claws and self.__ration == 'meat':
-            print(f'{self.__name} hunting on weaker than it animals and eat them')
+            print(f'{self._name} hunting on weaker than it animals and eat them')
         elif self.__ration == 'vegan' or 'omnivorous':
-            print(f'{self.__name} could eat berries and other vegan food')
+            print(f'{self._name} could eat berries and other vegan food')
 
+    @abstractmethod
+    def make_noise(self):
+        pass
