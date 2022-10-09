024c2710bbf62fd6f2218751aff1780eb042bb07
dianamokhnitskaya <112774353+dianamokhnitskaya@users.noreply.github.com>
2022-10-10 00:19:51 +0300

    Update HW6_task3.py

--- Diana_Mokhnitskaya/HW6/HW6_task3.py
+++ Diana_Mokhnitskaya/HW6/HW6_task3.py
@@ -3,7 +3,6 @@ from collections import Counter
 with open("text.txt") as file:
     new_string = file.read()
     print(new_string)
-    all_characters = re.findall(r"[a-zA-Z]\D\S", new_string)
-    print(Counter(new_string))
-
-# Не понимаю, как убрать символы и точки из подсчета
+    all_characters = re.findall(r"[A-z]", new_string)
+    print(Counter(all_characters))
+    
