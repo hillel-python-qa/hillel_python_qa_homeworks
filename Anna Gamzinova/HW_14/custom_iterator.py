942864080f46c26d91b5edeb5f2da61d9616e26d
gamzinova <gamzinova@gmail.com>
2022-10-11 14:29:07 +0300

    [HW14] custom iterator in progress

--- Anna Gamzinova/HW_14/custom_iterator.py
+++ Anna Gamzinova/HW_14/custom_iterator.py
@@ -1,3 +1,6 @@
+import math
+
+
 class CustomIterator:
     def __init__(self, sequence: list, start_index: int, end_index: int):
         self.__sequence = sequence
@@ -9,21 +12,22 @@ class CustomIterator:
 
     def __next__(self):
         if self.__start_index > self.__end_index:
-            print("Please double check your indexes. Your start index is higher thn the end index.")
-        if self.__start_index > len(self.__sequence):
-            print("Your start index is out of range")
+            raise ValueError("Please double check your indexes. Your start index is higher thn the end index.")
+        elif math.fabs(self.__start_index) > len(self.__sequence):
+            raise ValueError("Your start index is out of range")
+        elif math.fabs(self.__end_index) > len(self.__sequence):
+            raise ValueError("Your end index is out of range")
 
-
-        if self.__start_index < self.__end_index:
-            item = self.__sequence[self.__start_index]
+        elif self.__start_index < self.__end_index:
+            number = self.__sequence[self.__start_index]
             self.__start_index += 1
-            return item
+            return number
         else:
             raise StopIteration
 
 
 if __name__ == '__main__':
-    custom_iterator = CustomIterator([1, 2, 3, 4, 5], -1, )
+    custom_iterator = CustomIterator([1, 2, 3, 4, 5], -6, 4)
 
     for item in custom_iterator:
         print(item)
