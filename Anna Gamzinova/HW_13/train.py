dc9016e52f721370ea5b54626ad396b948210961
gamzinova <gamzinova@gmail.com>
2022-10-15 14:40:22 +0300

    [HW13] in progress

--- Anna Gamzinova/HW_13/train.py
+++ Anna Gamzinova/HW_13/train.py
@@ -1,4 +1,23 @@
-from wagons import Wagons
+from wagons import Wagon
 
-class Train(Wagons):
-    def __init(self,wagons):
+
+class Train(Wagon):
+    def __init__(self, wagons: int, passengers: int):
+        super().__init__(wagons, passengers)
+
+    def __len__(self):
+        return self.wagons
+
+    def add_wagon(self, new_wagon_number: int = 1):
+        """
+        A method to add the number of passengers
+        """
+        if type(new_wagon_number) == int and new_wagon_number > 0:
+            self.__wagons += new_wagon_number
+        else:
+            print("The new new wagon's number should be numeric  ")
+
+
+if __name__ == '__main__':
+    my_train = Train(10, 5)
+    print(len(my_train))
--- Anna Gamzinova/HW_13/wagons.py
+++ Anna Gamzinova/HW_13/wagons.py
@@ -1,7 +1,7 @@
-class Wagons:
-    def __init__(self, wagons: int, passengers: int):
-        if (type(wagons) == int) and wagons > 0:
-            self.__wagons = wagons
+class Wagon:
+    def __init__(self, wagon_number: int, passengers: int):
+        if (type(wagon_number) == int) and wagon_number > 0:
+            self.__wagon_number = wagon_number
         else:
             raise ValueError("The wagon's number should be numeric")
         if (type(passengers) == int) and passengers > 0:
@@ -10,13 +10,13 @@ class Wagons:
             raise ValueError("The passengers number should be numeric")
 
     @property
-    def wagons(self):
-        return self.__wagons
+    def wagon_number(self):
+        return self.__wagon_number
 
-    @wagons.setter
-    def wagons(self, new_wagons_number):
-        if (type(new_wagons_number) == int) and new_wagons_number > 0:
-            self.__wagons = new_wagons_number
+    @wagon_number.setter
+    def wagon_number(self, new_wagon_number):
+        if (type(new_wagon_number) == int) and new_wagon_number > 0:
+            self.__wagon_number = new_wagon_number
         else:
             print("The new wagon's number should be numeric and higher than 0")
 
@@ -38,15 +38,17 @@ class Wagons:
         if type(passenger_number) == int and passenger_number > 0 and (self.__passengers + passenger_number) < 11:
             self.__passengers += passenger_number
         else:
-            print("The new passenger's number should be numeric.")
+            print(
+                f"The new passenger's number should be numeric and total number should not exceed 10.\n"
+                f"Current number of passengers is {self.__passengers} ")
 
     def __len__(self):
         return self.__passengers
 
 
 if __name__ == '__main__':
-    wagon = Wagons(3, 5)
+    wagon = Wagon(3, 5)
     print(len(wagon))
-    wagon.add_passenger(2)
+    wagon.add_passenger(6)
+    wagon.add_passenger(3)
     print(wagon.passengers)
-
