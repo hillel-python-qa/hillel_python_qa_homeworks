e5a99ca0494c3d3bac883b0cfef966585f648046
anutka <azaychenko1999@gmail.com>
2022-10-15 10:16:16 +0300

    added template for proxy

---
+++ HannaPidchasiuk/HW15/proxy/txt_proxy_readed_writer.py
@@ -0,0 +1,16 @@
+from HannaPidchasiuk.HW15.proxy.txt_reader import TxtReader
+
+
+class TxtProxyWriterReader:
+    def __init__(self, file_path):
+        self.__result = ''
+        self.__is_actual = False
+        self.__txt_reader = TxtReader(file_path)
+
+    def read_file(self):
+        if self.__is_actual:
+            return self.__result
+        else:
+            self.__result = self.__txt_reader.read()
+            self.__is_actual = True
+            return self.__result
---
+++ HannaPidchasiuk/HW15/proxy/txt_reader.py
@@ -0,0 +1,12 @@
+from HannaPidchasiuk.HW15.proxy.reader import Reader
+
+
+class TxtReader(Reader):
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read(self):
+        with open(self.file_path, 'r') as file:
+            text = file.read()
+        return text
