596d78047016589ba71702150dcd5a28ed287faf
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Air.local>
2022-10-16 19:01:59 +0300

    Comments are added, a class is moved to the another file

---
+++ Oleksii_Fedorchuk/HW14_Oleksii_Fedorchuk/classes.py
@@ -0,0 +1,12 @@
+class Classes:
+    """ Polymorphism"""
+
+    def __init__(self, number_of_class: int, flor_in_the_school: int):
+        self.__number_of_class = number_of_class
+        self.__flor_in_the_school = flor_in_the_school
+
+    def number_of_class(self):
+        return self.__number_of_class
+
+    def flor_in_the_school(self):
+        return self.__flor_in_the_school
--- Oleksii_Fedorchuk/HW14_Oleksii_Fedorchuk/task1.py
+++ Oleksii_Fedorchuk/HW14_Oleksii_Fedorchuk/task1.py
@@ -1,3 +1,6 @@
+from Project_Git.hillel_python_qa_homeworks.Oleksii_Fedorchuk.HW14_Oleksii_Fedorchuk.classes import Classes
+
+
 class School:
     def __init__(self, school_number: int, director: str, secretary: str, number_of_students: int):
         self.__school_number = school_number
@@ -10,7 +13,7 @@ class School:
 
     @property
     def director(self):
-        """ Encapsulation"""
+        """ Encapsulation &  Hiding """
 
         return self.__director.capitalize()
 
@@ -28,21 +31,9 @@ class School:
                f"Total number of students in school is: {self.__number_of_students}\n"
 
 
-class Classes:
-    """ Polymorphism"""
-
-    def __init__(self, number_of_class: int, flor_in_the_school: int):
-        self.__number_of_class = number_of_class
-        self.__flor_in_the_school = flor_in_the_school
-
-    def number_of_class(self):
-        return self.__number_of_class
-
-    def flor_in_the_school(self):
-        return self.__flor_in_the_school
-
-
 class ClassOfMath(Classes):
+    """  Inheritance """
+
     def __init__(self, number_of_class: int, flor_in_the_school: int, subject: str):
         super(ClassOfMath, self).__init__(number_of_class, flor_in_the_school)
         self.__subject = subject
