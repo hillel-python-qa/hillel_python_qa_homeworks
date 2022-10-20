3b07b293e55c47000c4733db450bdcd5fdc8f0ac
Neitore <alekseymkaale@gmail.com>
2022-10-20 13:21:22 +0300

    Homework 13 Missing Files

--- Oleksii_Makarov/HW13/animal_1.py
+++ Oleksii_Makarov/HW13/animal_1.py
@@ -1,50 +1,15 @@
-import re
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
-    def __init__(self, species: str, habitation: str, size: str):
-        self.__species = species
-        self.__habitation = habitation
-        self.__size = size
-
-    @property
-    def species(self):
-        return self.__species
-
-    @species.setter
-    def species(self, species_name):
-        if re.search(r'\S', species_name):
-            self.__species = species_name
-        else:
-            raise NameError("Species name should be filled")
-
-    @property
-    def habitation(self):
-        return self.__habitation
-
-    @habitation.setter
-    def habitation(self, habitation_name):
-        if re.search(r'\S', habitation_name):
-            self.__species = habitation_name
-        else:
-            raise NameError("Habitation name should be filled")
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, size_value):
-        if re.search(r'\S', size_value):
-            self.__size = size_value
-        else:
-            raise NameError("Size value  be filled")
+    def __init__(self, habitation: str, reproduction: str):
+        self._habitation = habitation
+        self._reproduction = reproduction
 
     @abstractmethod
     def _i_eat(self):
         pass
 
     @abstractmethod
-    def _i_am_threatened(self):
+    def _i_am_reproducing(self):
         pass
--- Oleksii_Makarov/HW13/mammal_2.py
+++ Oleksii_Makarov/HW13/mammal_2.py
@@ -1,4 +1,45 @@
-from .Oleksii_Makarov.HW13.(1)animal import Animal
+from animal_1 import Animal
+import re
 
 
 class Mammal(Animal):
+    def __init__(self, reproduction: str, habitation: str, food: str):
+        super().__init__(habitation, reproduction)
+        self._food = food
+
+    def _i_am_reproducing(self):
+        """
+            Prints phrase using protected attribute reproduction
+        """
+        print(f'I am reproducing using {self._reproduction}')
+
+    def _i_eat(self):
+        """
+            Prints phrase using protected attribute food
+        """
+        print(f'I eat {self._food}')
+
+    def _i_live(self):
+        """
+           Prints phrase using protected attribute habitation
+        """
+        print(f'I live in {self._habitation}')
+
+    def reproducing(self):
+        """
+            Uses protected method _i_am_reproducing
+        """
+        self._i_am_reproducing()
+
+    def eating(self):
+        """
+            Uses protected method _i_eat
+        """
+        self._i_eat()
+
+    def living(self):
+        """
+            Uses protected method _i_live
+        """
+        self._i_live()
+
