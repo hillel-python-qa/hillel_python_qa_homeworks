a9390c6dcee4bfce91be840b50bc1a412b041409
Main <natalybon1405@gmail.com>
2022-10-21 00:43:32 +0300

    HW_12_interfaces_Nataly

---
+++ Nataly_Bondarenko/HW_12/Ipost_processing.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+
+class IPost_processing(ABC):
+
+    @abstractmethod
+    def sort_photos(self):
+        pass
+
+    @abstractmethod
+    def send_photos_to_client(self):
+        pass
+
+    @abstractmethod
+    def get_money(self):
+        pass
