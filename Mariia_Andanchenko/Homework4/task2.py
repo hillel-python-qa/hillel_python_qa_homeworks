2e7fec94412828c48f1a26509e1ca5f4ad7562d2
Andanchenko Maria <andanchenko.maria@gmail.com>
2022-10-30 01:25:35 +0300

    Homework4_Andanchenko

---
+++ Mariia_Andanchenko/Homework4/task2.py
@@ -0,0 +1,8 @@
+number = 2
+
+multiply1 = number * 2
+multiply2 = number << 1
+print(f'{number} multiplied by 2 is {multiply1} or {multiply2}')
+div1 = number / 2
+div2 = number >> 1
+print(f'{number} divide by 2 is {div1} or {div2}')
