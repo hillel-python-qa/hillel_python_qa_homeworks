445fc0c2f94c35dd2dafece1a66b70d7b2817428
AndriyPovzun <povzuun99@gmail.com>
2022-11-06 14:01:00 +0200

    fix_some_test_and_add_positive

--- Andriy_Povzun/HW20/test_human/test_human_class.py
+++ Andriy_Povzun/HW20/test_human/test_human_class.py
@@ -1,4 +1,9 @@
 import pytest
+import random
+import sys, os
+from human import Human
+
+sys.path.append(os.path.abspath('../HW20'))
 
 
 @pytest.mark.xfail(reason='No age validation. Age cannot be less than zero. BUG')
@@ -10,25 +15,24 @@ def test_create_human_with_negative_age(create_human_negative_age):
 
 @pytest.mark.negative
 def test_error_when_set_same_gender(create_valid_human):
-    human = create_valid_human
+    human, name = create_valid_human
     gender = human.gender
-    with pytest.raises(Exception):
+    with pytest.raises(Exception, match=f"{name} already has gender '{gender}'"):
         human.change_gender(gender)
 
 
 @pytest.mark.negative
-def test_change_gender_on_invalid(create_valid_human):
-    human = create_valid_human
-    with pytest.raises(Exception):
-        human.change_gender('bla')
+def test_change_gender_on_invalid(create_valid_human, faker):
+    human, name = create_valid_human
+    with pytest.raises(Exception, match='Not correct name of gender'):
+        human.change_gender(name)
 
 
-@pytest.mark.negative
-def test_human_grow_after_100_year(human_grow_to_100_years):
-    human = human_grow_to_100_years
+@pytest.mark.smoke
+def test_human_death_after_100_year(human_have_100_years):
+    human, name = human_have_100_years
     human.grow()
-    assert human.age == 100
-    with pytest.raises(Exception):
+    with pytest.raises(Exception, match=f'{name} is already dead...'):
         human.grow()
 
 
@@ -40,16 +44,16 @@ def test_human_with_number_name(create_human_invalid_name):
 
 
 @pytest.mark.smoke
-def test_success_change_human_gender(create_valid_human):
-    male = 'male'
-    female = 'female'
-    human = create_valid_human
-    if human.gender == male:
-        human.change_gender(female)
-        assert human.gender == female
-    else:
-        human.change_gender(male)
-        assert human.gender == male
+@pytest.mark.parametrize("gender", ['male', 'female'], ids=['from male to female', 'from female to male'])
+def test_success_change_human_gender(access_to_genders, gender, faker):
+    genders = access_to_genders
+    human = Human(faker.name(), random.randint(0, 100), gender)
+    gender_before_change = human.gender
+    for element in genders:
+        if element != human.gender:
+            human.change_gender(element)
+            break
+    assert gender_before_change != human.gender
 
 
 @pytest.mark.xfail(reason='No class initialization validation, BUG')
@@ -62,13 +66,21 @@ def test_validation_during_initialization_instance(create_empty_human_instance):
 @pytest.mark.negative
 def test_grow_human_with_string_age(create_human_with_string_age):
     human = create_human_with_string_age
-    with pytest.raises(Exception):
+    with pytest.raises(TypeError, match="'<' not supported between instances of 'str' and 'int'"):
         human.grow()
 
 
 @pytest.mark.smoke
 def test_check_grow_func(create_valid_human):
-    human = create_valid_human
+    human, name = create_valid_human
     age_before_func = human.age
     human.grow()
-    assert human.age == age_before_func + 1
+    assert human.age == age_before_func + 1, 'Function work wrong\n' \
+                                             'Expected: after using grow() func, human instance ' \
+                                             'will be older on 1 year'
+
+
+@pytest.mark.smoke
+def test_age(create_valid_human):
+    human, name = create_valid_human
+    assert 0 < human.age < 101, 'Human age are not valid!'
