1a3b02fcd1e8a02cdd2084dba10b927e7bc47629
andrii-voinenko <voynenko.andrey@gmail.com>
2022-11-09 22:33:30 +0200

    hw16_fixes_according_to_comments

--- Andrii_Voinenko/Lesson20/human_class_tc/test_human.py
+++ Andrii_Voinenko/Lesson20/human_class_tc/test_human.py
@@ -12,33 +12,34 @@ def test_grow_up(create_custom_human_instance):
 
 @pytest.mark.skip(reason='No validation for negative number')
 def test_negative_age(create_custom_human_instance):
-    human = create_custom_human_instance('May', -2, 'female')
-    with pytest.raises(Exception):
-        human.grow()
+    with pytest.raises(ValueError, match='Human cannot have negative age'):
+        """
+        Use as a check creating instance because on this step should be validation
+        """
+        human = create_custom_human_instance('May', -2, 'female')
 
 
 def test_str_instead_int_for_age(create_custom_human_instance):
     human = create_custom_human_instance('Olaf', '19', 'male')
-    with pytest.raises(TypeError):
-        human.grow()
+    assert pytest.raises(TypeError)
 
 
 def test_grow_up_for_dead_human(human_dead_status):
     human = human_dead_status
-    with pytest.raises(Exception):
+    with pytest.raises(Exception, match=f'{human._Human__name} is already dead...'):
         human.grow()
 
 
 @pytest.mark.parametrize('wrong_gender', ['Male', 'Female', 1, '1'])
 def test_gender_validation_with_alive_status(create_custom_human_instance, wrong_gender):
     human = create_custom_human_instance('Joan', 20, 'female')
-    with pytest.raises(Exception):
+    with pytest.raises(Exception, match='Not correct name of gender'):
         human.change_gender(wrong_gender)
 
 
 def test_gender_validation_with_dead_status(human_dead_status):
     human = human_dead_status
-    with pytest.raises(Exception):
+    with pytest.raises(Exception, match=f'{human._Human__name} is already dead...'):
         human.change_gender('female')
 
 
@@ -55,3 +56,19 @@ def test_change_gender(create_default_human):
     assert human.gender == 'male', (f'Gender was not changed\n'
                                     f'Actual result: {change_gender}\n'
                                     f'Expected result: male')
+
+
+def test_gender_property(create_default_human):
+    human = create_default_human
+
+    assert human.gender == 'female', (f'New gender value was not set\n'
+                                    f'Actual result: {human.gender}\n'
+                                    f'Expected result: male')
+
+
+def test_age_property(create_default_human):
+    human = create_default_human
+
+    assert human.age == 55, (f'New age value was not set\n'
+                             f'Actual result: {human.age}\n'
+                             f'Expected result: 15')
